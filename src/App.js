53aaa794ac638efcc28dc5138dcef2f902fd0317
Zak <zak.puthoff@gmail.com>
2018-12-16 16:03:19 -0600

    Sets up the basic functionality of App.js
    Here I put in the state data, updating methods, data cleanup, and basic routing

--- src/App.js
+++ src/App.js
@@ -15,39 +15,91 @@ class BooksApp extends Component {
     featuredBook: {}
   }
 
-  fixer = (someBooks) => {
+  fixer = (someBooks, type) => {
+    const { bookRepository } = this.state;
+    let updatedBookRepository, updatedSearchResults, updatedFeaturedBook;
 
-    return someBooks.map(book => {
-
+    const updatedSomeBooks = someBooks.map(book => {
       if (!book.title) { book.title = 'Unknown' };
       if (!book.authors) { book.authors = ['Unknown'] };
       if (!book.imageLinks) { book.imageLinks = {} }
       if (!book.imageLinks.thumbnail) { book.imageLinks.thumbnail = 'http://books.google.com/books/content?id=NLK2AAAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api' };
       if (!book.shelf) { book.shelf = 'none'};
       if (!book.selected) { book.selected = false };
-
-      book.checked = false;
       
       return book;
     });
+
+    switch(type) {
+      case 'bookRepository': {
+        updatedBookRepository = updatedSomeBooks;
+        this.setState({bookRepository: updatedBookRepository})
+        break;
+      }
+      case 'searchResults': {
+        updatedSearchResults = updatedSomeBooks.map(someBook => {
+          return bookRepository.map(book => {
+            if (someBook.id === book.id) {someBook.shelf = book.shelf};
+            return someBook;
+          })
+        })
+        this.setState({searchResults: updatedSearchResults});      
+        break;
+      }
+      case 'featuredBook': {
+        updatedFeaturedBook = updatedSomeBooks.map(someBook => {
+          return bookRepository.map(book => {
+            if (someBook.id === book.id) {someBook.shelf = book.shelf};
+            return someBook;
+          })
+        })
+        this.setState({featuredBook: updatedFeaturedBook})
+        break;
+      }
+      default: { return }
+    }
   }
 
-  updateBookRepository = () => {
+  updateBookRepository = async (bookID, shelfID) => {
+    const { bookRepository } = this.state;
 
+    let booksToUpdate = bookRepository.filter(book => (book.selected === true)).push({id: bookID, shelf: shelfID});
+    
+    await booksToUpdate.forEach(book => { BooksAPI.update({ id: book.id }, shelfID) })
+
+    BooksAPI.getAll().then(fetchedBooks => this.fixer(fetchedBooks, 'bookRepository'));
   }
 
-  updateSearchResults = () => {
+  updateSearchResults = async (userQuery) => {
+    if (userQuery.length === 0) {return};
 
+    let initialResults = await BooksAPI.search(userQuery);
+    initialResults = (initialResults.error) ? ([]) : initialResults;
+    this.fixer(initialResults, 'searchResults');
   }
 
-  updateFeaturedBook = () => {
-
+  updateFeaturedBook = async (bookID, shelfID) => {
+    const requestedBook = await BooksAPI.get(bookID);
+    this.fixer(requestedBook, 'featuredBook')
   }
 
   render() {
 
     return (
-      <div/>
+      <div>
+        <Route exact path='/' render={(props) => (
+          <Bookshelves {...props} />
+        )}/>
+        
+        <Route path='/search' render={(props) => (
+          <Searchpage {...props} />
+        )}/>
+
+        <Route path='/featuredBook/:bookID' render={(props) => (
+          <Featuredbook {...props} />
+        )}/>
+      
+      </div>
     )
   }
 }
